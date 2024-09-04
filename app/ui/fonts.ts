0a776154cf3940b077a2a55f9b37beda460e25d3
leonn <leon@rain.co.za>
2024-09-05 01:06:12 +0200

    update initial project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Poppins, Lusitana } from 'next/font/google';
+ 
+export const inter = Poppins({ weight: ['100','200', '300', '400','500', '700','900'], subsets: ['latin'] });
+export const lusitana = Lusitana ({ weight: ['400', '700'], subsets: ['latin'] });
